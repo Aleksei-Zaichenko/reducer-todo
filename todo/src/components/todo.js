70e450e904ddba6c8c76eb1751bce8db0b894d97
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-03-16 16:39:09 -0400

    completed step 2

--- todo/src/components/todo.js
+++ todo/src/components/todo.js
@@ -1,7 +1,18 @@
 import React, { useState, useReducer } from 'react';
+
 import { initialState, toDoReducer} from '../reducers/toDoReducer';
 
-export const toDo = () =>{
+const ToDo = () => {
     const [state, dispatch] = useReducer(toDoReducer, initialState);
-    
-}
+    const [newToDoTask, setNewToDoTask] = useState('');
+
+    return(
+        <div className="App">
+            <h3>
+                {state.item}
+            </h3>
+        </div>
+    );
+};
+
+export default ToDo;
--- todo/src/reducers/toDoReducer.js
+++ todo/src/reducers/toDoReducer.js
@@ -2,8 +2,8 @@ export const initialState = {
         item: 'Learn about reducers',
         completed: false,
         id: 3892987589
-}
+};
 
-export const toDoReducer =(state, action) => {
+export const toDoReducer = (state, action) => {
     return state;
-}
+};
